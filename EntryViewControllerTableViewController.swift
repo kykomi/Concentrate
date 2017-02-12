31e3248377a747f549a6ae752954a3546c2301a2
Tsukasa Komiyama <apps.by.komi@gmail.com>
2017-02-12 14:01:18 +0900

    Show sample data in tableview

---
+++ EntryViewControllerTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  EntryViewControllerTableViewController.swift
+//  Concentrate
+//
+//  Created by 小宮山 司 on 2017/02/12.
+//  Copyright © 2017年 Tsukasa K. All rights reserved.
+//
+
+import UIKit
+
+class EntryViewControllerTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "entry", for: indexPath)
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
